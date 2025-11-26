e7105dd7cb1740e0582f3ceaa6d0d66cbe45830f
Gabin BLOQUET <gabin.blqt@gmail.com>
2025-11-26 12:10:30 +0100

    fixes with AI

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function ($) {
   "use strict";
 
+  // Preloader
   var initPreloader = function () {
     $(document).ready(function () {
       var Body = $('body');
@@ -12,52 +13,8 @@
     });
   }
 
-  // init Chocolat light box
-  var initChocolat = function () {
-    Chocolat(document.querySelectorAll('.image-link'), {
-      imageSize: 'contain',
-      loop: true,
-    })
-  }
-
   $(document).ready(function () {
-    // Isotope Initialization
-    var $container = $('.isotope-container').isotope({
-      itemSelector: '.item',
-      layoutMode: 'masonry',
-    });
-
-    // Filter items on button click
-    $('.filter-button').click(function () {
-      var filterValue = $(this).attr('data-filter');
-      if (filterValue === '*') {
-        $container.isotope({ filter: '*' });
-      } else {
-        $container.isotope({ filter: filterValue });
-      }
-      $('.filter-button').removeClass('active');
-      $(this).addClass('active');
-    });
-
-    // Video Modal
-    var $videoSrc;
-    $('.play-btn').click(function () {
-      $videoSrc = $(this).data("src");
-    });
-
-    $('#myModal').on('shown.bs.modal', function (e) {
-      $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
-
-    $('#myModal').on('hide.bs.modal', function (e) {
-      $("#video").attr('src', $videoSrc);
-    })
-
-    // Swiper Initialization
-    var sliderSwiper = new Swiper(".slider", {
-      effect: "fade",
-    });
-
+    // Swiper Initialization - Room Swiper (utilisé sur index.html)
     var roomSwiper = new Swiper(".room-swiper", {
       slidesPerView: 3,
       spaceBetween: 20,
@@ -78,38 +35,8 @@
       },
     });
 
-    var gallerySwiper = new Swiper(".gallery-swiper", {
-      effect: "fade",
-      navigation: {
-        nextEl: ".main-slider-button-next",
-        prevEl: ".main-slider-button-prev",
-      },
-    });
-
-    var thumbSlider = new Swiper(".product-thumbnail-slider", {
-      autoplay: true,
-      loop: true,
-      spaceBetween: 8,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-
-    var largeSlider = new Swiper(".product-large-slider", {
-      autoplay: true,
-      loop: true,
-      spaceBetween: 10,
-      effect: 'fade',
-      thumbs: {
-        swiper: thumbSlider,
-      },
-    });
-
     // Preloader
     initPreloader();
-
-    // Chocolat
-    initChocolat();
 
     // Animate on Scroll
     AOS.init({
